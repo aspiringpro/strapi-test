3a6aacd9a3dfb695219d6cf07024be4286998ef2
aspiringpro <marcusjamesreed@gmail.com>
2021-11-16 04:24:26 +0000

    first commit

---
+++ my-project/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '1d6f022745fa73286d528b47158cb3b4'),
+    },
+  },
+});
---
+++ my-project/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '4ceede5f-65fa-4a40-8875-9347696d225a'
+};
